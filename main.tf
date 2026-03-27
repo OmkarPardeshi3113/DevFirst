9eeb1fc8260516205c1bcb748b1492dae90d2e54
OmkarPardeshi3113 <110240418+OmkarPardeshi3113@users.noreply.github.com>
2026-03-27 11:02:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,21 +2,17 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 3.0.1"
+      version = "~> 3.9.0" # <--- This is the fix
     }
   }
 }
 
-# Tell Terraform to communicate with your local Docker engine
 provider "docker" {}
 
-# Define the container we want to run
 resource "docker_container" "portfolio" {
   image = "omkar-portfolio:latest"
   name  = "portfolio-container"
-  
-  # If a container with this name exists, Terraform will destroy it and recreate it with the new image
-  rm = true 
+  rm    = true 
 
   ports {
     internal = 80
